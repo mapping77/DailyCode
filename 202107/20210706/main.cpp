f30c597bcff6c874bdade054dc5b66a7f6c8b173
mapping77 <pcheng77@126.com>
2021-07-06 13:39:42 +0800

    add 20210706

---
+++ 202107/20210706/main.cpp
@@ -0,0 +1,23 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+    /*
+    //定义在函数体内的内置类型，没执行默认初始化，在这个版本编译器没报错。
+    //func();
+    double a_in;
+    cout << a_in << endl;
+    */
+
+
+
+    //列表初始化存在信息丢失会报错。
+    //int a={1.2};
+    //cout << a << endl; 
+
+
+
+    return 0;
+}
