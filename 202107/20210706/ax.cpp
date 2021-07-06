d6b007dcb176a38ea78ba26afaed37c6600d689a
mapping77 <pcheng77@126.com>
2021-07-06 16:48:40 +0800

    add extern const int gl=10

---
+++ 202107/20210706/ax.cpp
@@ -0,0 +1,9 @@
+
+
+extern const int gl=10;//这样linker可以找得到
+
+//const int gl=10;这样linker就找不到。
+
+void func(void){
+    ;
+}
--- 202107/20210706/main.cpp
+++ 202107/20210706/main.cpp
@@ -2,6 +2,7 @@
 
 using namespace std;
 
+extern const int gl;
 
 int main(){
     /*
@@ -11,13 +12,17 @@ int main(){
     cout << a_in << endl;
     */
 
-
-
     //列表初始化存在信息丢失会报错。
     //int a={1.2};
     //cout << a << endl; 
 
+    //
+    //const int a = 10;
+    //const int *p = &a;
+    //cout << p << endl;
 
 
+    
+    cout << gl << endl;
     return 0;
 }
